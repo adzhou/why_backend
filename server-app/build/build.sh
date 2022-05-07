66700c52213a6ffa77898031f88375a7a9292748
adzhou <adamus@agilesun.com>
2022-05-07 16:58:27 +0800

    add build and deploy files

---
+++ server-app/build/build.sh
@@ -0,0 +1,17 @@
+
+which node
+which npm 
+which cnpm
+
+node -v
+npm -v 
+cnpm -v
+
+
+# input your command here
+if [ ! -d "/opt/release" ]; then
+  mkdir /opt/release
+fi
+
+cnpm install
+
---
+++ server-app/deploy/deploy.sh
@@ -0,0 +1,17 @@
+
+pwd
+ls -l /var/www/
+ls -l /etc/nginx/
+cat /etc/nginx/nginx.config
+netstat -antp | grep 8090
+
+netstat -antp | grep 80
+
+netstat -antp | grep 8080
+
+netstat -antp | grep 8001
+
+
+#tar zxvf /opt/release/wohenyou/frontendpackage.tgz -C /var/www/release/wohenyou
+#sh /var/www/release/wohenyou/deploy.sh restart
+#   systemctl restart nginx
